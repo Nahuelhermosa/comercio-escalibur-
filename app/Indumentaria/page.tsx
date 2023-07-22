1ca1fcf1943fa5a351d7f00a043fc2ce8ec17bd1
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2023-07-21 23:11:55 -0300

    #

--- app/Indumentaria/page.tsx
+++ app/Indumentaria/page.tsx
@@ -7,47 +7,11 @@ import CardWrapper from '../componentes/CardWrapper';
 export default function Indumentaria() {
     return (
       <div className="producto">
-      <CardWrapper/>
+      
         <h1>stock</h1>
         <br />
-      
+      <CardWrapper/>
         <br />
-      <div className='grillas'>
-        <div className="card">
-          <img src="pacman.jpg" width={300} alt="Descripción de la imagen" />
-          <div className="card-content">
-            <h2>Remera pacman</h2>
-            <p>remera 100%</p>
-            <div className="card-info-detalles">
-                    <p className='precio'>$15000</p>
-                    <p className='envio'>Envío gratis</p>
-            </div>     
-          </div>
-        </div>
-
-        <div className="card">
-          <img src="pacman.jpg" width={300} alt="Descripción de la imagen" />
-          <div className="card-content">
-            <h2>Remera pacman</h2>
-            <p>remera 100%</p>
-            <div className="card-info-detalles">
-                    <p className='precio'>$15000</p>
-                    <p className='envio'>Envío gratis</p>
-            </div>     
-          </div>
-        </div>
-        <div className="card">
-          <img src="pacman.jpg" width={300} alt="Descripción de la imagen" />
-          <div className="card-content">
-            <h2>Remera pacman</h2>
-            <p>remera 100%</p>
-            <div className="card-info-detalles">
-                    <p className='precio'>$15000</p>
-                    <p className='envio'>Envío gratis</p>
-            </div>     
-          </div>
-        </div>
-    </div>
-  </div>
- );
+      </div>
+   )
 }
--- app/componentes/CardWrapper.tsx
+++ app/componentes/CardWrapper.tsx
@@ -1,21 +1,16 @@
 import Card from "./Card/Card"
 import './CardWrapper.css'
+import data from "./data_tarjetas.json"
 export default function CardWrapper () {
-    const producto = {
-
-        imagen: 'pacman.jpg',
-        titulo: 'remera',
-        descripcion: 'remera 100% algodon',
-        precio: 15000,
-        envio: true
-    }
+    
 
     return (
          <div className="wrapper"> 
          <h2>Variedad de indumentaria</h2>
          <div className="grilla">
-            <Card producto={producto}/>
-           
+            {data.map(elemento =>(
+            <Card producto={elemento}/>
+            ))}
          </div>
          </div>
     )
