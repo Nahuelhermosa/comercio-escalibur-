5b208022cd12eb18704804f03bfc16ad8f3e0b38
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2023-07-23 20:44:04 -0300

    #

--- app/componentes/Card/Card.tsx
+++ app/componentes/Card/Card.tsx
@@ -1,24 +1,30 @@
-import './Card.css'
+import './Card.css';
 
-export default function Card ({producto}) {
+// Define la interfaz para el tipo de datos de 'producto'
+interface Producto {
+  imagen: string;
+  titulo: string;
+  descripcion: string;
+  precio: number;
+  envio?: boolean;
+}
 
-    return (
-
-        <div className='grillas'>
-        <div className="card">
-          <img src={producto.imagen} width={290} alt="" />
-          <div className="card-content">
-            <h2>{producto.titulo}</h2>
-            <p>{producto.descripcion}</p>
-            <div className="card-info-detalles">
-                    <p className='precio'>${producto.precio}</p>
-                    {producto.envio &&
-                    <p className='envio'>Envio gratis</p>
-                    }
-            </div>      
+export default function Card({ producto }: { producto: Producto }) {
+  return (
+    <div className='grillas'>
+      <div className="card">
+        <img src={producto.imagen} width={290} alt="" />
+        <div className="card-content">
+          <h2>{producto.titulo}</h2>
+          <p>{producto.descripcion}</p>
+          <div className="card-info-detalles">
+            <p className='precio'>${producto.precio}</p>
+            {producto.envio &&
+              <p className='envio'>Envío gratis</p>
+            }
           </div>
         </div>
-        </div>
-    )
-
+      </div>
+    </div>
+  );
 }
