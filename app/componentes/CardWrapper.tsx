84023f6923e5b8c8327e5c8e75c4325cdc569478
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2023-07-28 23:23:15 -0300

    #

--- app/componentes/CardWrapper.tsx
+++ app/componentes/CardWrapper.tsx
@@ -1,13 +1,28 @@
+"use client"
+import { useState } from "react"
 import Card from "./Card/Card"
 import './CardWrapper.css'
 import data from "./data_tarjetas.json"
-export default function CardWrapper () {
-    
 
+
+export default function CardWrapper () { 
+    const [ filtros, setfiltros] = useState("");
     return (
          <div className="wrapper"> 
          <h2>Variedad de indumentaria</h2>
          <div className="grilla">
+            <div>
+                <label>
+                <input type="checkbox" name="remera" value="remera" onChange={()=>{
+                    setfiltros("remera")
+                }}/> remera < br/>
+                </label>
+                <label >
+                <input type="checkbox" name="lombor" value="lombor" onChange={()=>{
+                    setfiltros("lombor")
+                }}/> lombor < br/>
+                </label>
+            </div>    
             {data.map(elemento =>(
             <Card key={elemento.id} producto={elemento}/>
             ))}
