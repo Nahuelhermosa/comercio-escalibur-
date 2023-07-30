9f7cce91d85c6159d1cb38b1ec4dc9b5306d9832
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2023-07-30 01:48:07 -0300

    #

--- app/componentes/CardWrapper.tsx
+++ app/componentes/CardWrapper.tsx
@@ -6,26 +6,41 @@ import data from "./data_tarjetas.json"
 
 
 export default function CardWrapper () { 
-    const [ filtros, setfiltros] = useState("");
+    const [ filtros, setfiltros] = useState("all");
     return (
          <div className="wrapper"> 
-         <h2>Variedad de indumentaria</h2>
+         <h2 className="titulowrapper">Variedad de indumentaria</h2>
          <div className="grilla">
             <div>
                 <label>
-                <input type="checkbox" name="remera" value="remera" onChange={()=>{
+                <input type="radio" name="categoria" value="remera" onChange={()=>{
                     setfiltros("remera")
                 }}/> remera < br/>
                 </label>
                 <label >
-                <input type="checkbox" name="lombor" value="lombor" onChange={()=>{
+                <input type="radio" name="categoria" value="lombor" onChange={()=>{
                     setfiltros("lombor")
                 }}/> lombor < br/>
                 </label>
+                <label >
+                <input type="radio" name="categoria" value="consola" onChange={()=>{
+                    setfiltros("consola")
+                }}/> consola < br/>
+                </label>
             </div>    
-            {data.map(elemento =>(
-            <Card key={elemento.id} producto={elemento}/>
-            ))}
+            
+           {data.map(elemento =>{
+            if(filtros === "all"){
+                return(
+                    <Card key={elemento.id} producto={elemento}/>
+                )}
+            if(elemento.categoria === filtros) {
+                return(
+                    <Card key={elemento.id} producto={elemento}/>
+                    )
+                }
+               }
+            )}
          </div>
          </div>
     )
