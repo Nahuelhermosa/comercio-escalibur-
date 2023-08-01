bcc645210d13dc9fb956e108dc8ab7fc65ea1d25
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2023-07-31 22:55:45 -0300

    #

--- app/servicio/page.tsx
+++ app/servicio/page.tsx
@@ -9,8 +9,10 @@ export default function Servicios () {
             <p>Bien venidos a Escalibur "el calaboso del nerd" somos una tienda que se centra en el diseño y venta de ropa para hombre, mujeres, niñas y niño, presentando un gran impacto visual, con ideas alternativas dedicado a la cultura pop, anime, manga y sus variantes.
                  </p>
             <br />
+            <div className="contenedor-imagenes">
             <img className='imagen-servicio' src="lombor.jpg" alt="indumetaria" />
             <img className='imagen-servicio' src="nirvana.jpg" alt="indumentaria" />
+            </div>
                 </div>
         </div>
     )
