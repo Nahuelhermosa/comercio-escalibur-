d79d4f58753e5e3967787a26fd505c2e6ef2f051
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2025-03-02 11:26:17 -0300

    ultimo

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,21 @@ export default function Home() {
             ESCALIBUR "el calabozo del nerd" somos una tienda que se centra en ventas de consolas nuevas como retro, deportes urbanos, en diseño y venta de ropa para hombre, mujeres, niñas y niños, presentando un gran impacto visual, con ideas alternativas dedicadas a la cultura pop, anime, manga y sus variantes.
           </h2>
         </div>
+        
       </div>
+      <div className="servicio">
+            <h1 className="servicio-title">El calaboso del nerd</h1>
+            <br />
+                <div className="servicio-contenedor">                    
+            <p>Bienvenidos a Escalibur "el calaboso del nerd" somos una tienda que se centra en el diseño y venta de ropa para hombre, mujeres, niñas y niño, presentando un gran impacto visual, con ideas alternativas dedicado a la cultura pop, anime, manga y sus variantes.
+                 </p>
+            <br />
+            <div className="contenedor-imagenes">
+            <img className='imagen-servicio' src="lombor.jpg" alt="indumetaria" />
+            <img className='imagen-servicio' src="nirvana.jpg" alt="indumentaria" />
+            </div>
+                </div>
+        </div>
     </main>
   );
 }
